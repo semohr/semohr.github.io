7f63dcf8b7a21b8af9b2e08c613e4ace01a9b952
Sebastian <sebastian@mohrenclan.de>
2020-08-08 21:32:12 +0200

    Added sections: Resume, skills, my works and contact. Added some content and TODO labels

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 * @Author: Sebastian B. Mohr
 * @Date:   2020-08-07 17:18:28
 * @Last Modified by:   Sebastian
-* @Last Modified time: 2020-08-08 14:32:54
+* @Last Modified time: 2020-08-08 19:02:27
 */
 
 
@@ -14,7 +14,6 @@ var navbar_btn =  document.getElementsByClassName("mobile-navigator-toggle")
 
 navbar_btn[0].onclick = function(e){
   //Toggle header class
-  console.log("hi")
   header = document.getElementById("header")
   header.classList.toggle("mobile-navigator-active");
   //Repalce icon with cross (x)
@@ -124,3 +123,93 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+/*--------------------------------------------------
+# Active navigation item
+# Could have done the for loop myself but short amount of google does the
+# job too.
+# https://stackoverflow.com/questions/32395988/highlight-menu-item-when-scrolling-down-to-section
+----------------------------------------------------*/
+
+// cache the navigation links 
+var $navigationLinks = document.querySelectorAll('nav > ul > li > a');
+// cache (in reversed order) the sections
+var $sections = document.getElementsByTagName('section');
+
+// map each section id to their corresponding navigation link
+var sectionIdTonavigationLink = {};
+for (var i = $sections.length-1; i >= 0; i--) {
+  var id = $sections[i].id;
+  sectionIdTonavigationLink[id] = document.querySelectorAll('nav > ul > li > a[href=\\#' + id + ']') || null;
+}
+
+// throttle function, enforces a minimum time interval
+function throttle(fn, interval) {
+  var lastCall, timeoutId;
+  return function () {
+    var now = new Date().getTime();
+    if (lastCall && now < (lastCall + interval) ) {
+      // if we are inside the interval we wait
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(function () {
+        lastCall = now;
+        fn.call();
+      }, interval - (now - lastCall) );
+    } else {
+      // otherwise, we directly call the function 
+      lastCall = now;
+      fn.call();
+    }
+  };
+}
+
+function getOffset( el ) {
+  var _x = 0;
+  var _y = 0;
+  while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+    _x += el.offsetLeft - el.scrollLeft;
+    _y += el.offsetTop - el.scrollTop;
+    el = el.offsetParent;
+  }
+  return { top: _y, left: _x };
+}
+
+function highlightNavigation() {
+  // get the current vertical position of the scroll bar
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  // iterate the sections
+  for (var i = $sections.length-1; i >= 0; i--) {
+    var currentSection = $sections[i];
+    // get the position of the section
+    var sectionTop = getOffset(currentSection).top;
+
+     // if the user has scrolled over the top of the section  
+    if (scrollPosition >= sectionTop - 250) {
+      // get the section id
+      var id = currentSection.id;
+      // get the corresponding navigation link
+      var $navigationLink = sectionIdTonavigationLink[id];
+      // if the link is not active
+      if (typeof $navigationLink[0] !== 'undefined') {
+        if (!$navigationLink[0].classList.contains('active')) {
+          // remove .active class from all the links
+          for (i = 0; i < $navigationLinks.length; i++) {
+            $navigationLinks[i].className = $navigationLinks[i].className.replace(/ active/, '');
+          }
+          // add .active class to the current link
+          $navigationLink[0].className += (' active');
+        }
+      } else {
+          // remove .active class from all the links
+          for (i = 0; i < $navigationLinks.length; i++) {
+            $navigationLinks[i].className = $navigationLinks[i].className.replace(/ active/, '');
+          }
+      } 
+      // we have found our section, so we return false to exit the each loop
+      return false;
+    }
+  }
+}
+
+window.addEventListener('scroll',throttle(highlightNavigation,150));
