39fbbff99d835321611e724a9ed1bd917ed8f9b2
Sebastian <sebastian@mohrenclan.de>
2020-10-18 14:38:38 +0200

    Added accordion view for work items. Also added link to covid germany website

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 * @Author: Sebastian B. Mohr
 * @Date:   2020-08-07 17:18:28
 * @Last Modified by:   Sebastian
-* @Last Modified time: 2020-08-10 16:53:31
+* @Last Modified time: 2020-10-18 13:33:38
 */
 
 
@@ -233,3 +233,57 @@ function highlightNavigation() {
 
 window.addEventListener('scroll',throttle(highlightNavigation,150));
 
+
+/*--------------------------------------------------------------
+# Accordion
+--------------------------------------------------------------*/
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
+
+/*--------------------------------------------------------------
+# Include html
+--------------------------------------------------------------*/
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
